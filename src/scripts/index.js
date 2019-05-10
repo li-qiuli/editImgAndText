07417cce332d686f9d55ce1aa89e426248813cd0
epub <epub@userdeMacBook-Pro.local>
2019-05-10 13:23:38 +0800

    add list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,32 @@
 import '../styles/index.scss';
+import '../thirdparty_css/jquery.fancybox.min.css';
+import '../thirdparty_css/font-awesome.min.css';
+import '../thirdparty_css/animate.css';
+import '../thirdparty_css/bootstrap.min.css';
 
+import '../css/pack.built.b4016507.cache.css';
+
+import '../thirdparty_js/object-assign.min.js';
+import '../thirdparty_js/promise.js';
+
+import '../thirdparty_js/jquery.min.js';
+import '../thirdparty_js/bootstrap.min.js';
+import '../thirdparty_js/lunr.min.js';
+import '../thirdparty_js/jquery.fancybox.min.js';
+import '../thirdparty_js/headroom.min.js';
+import '../thirdparty_js/jQuery.headroom.min.js';
+import '../thirdparty_js/jarallax.min.js';
+import '../thirdparty_js/greensock/TweenMax.min.js';
+import '../thirdparty_js/greensock/plugins/DrawSVGPlugin.min.js';
+import '../thirdparty_js/greensock/plugins/MorphSVGPlugin.min.js';
+import '../thirdparty_js/vue-bundle.min.js';
+
+
+import '../js/vendor_c.bundle.built.47020bb2.cache.js';
+import '../js/pack.built.b8527fde.cache.js';
+import '../js/cst.built.426f7fd1.cache.js';
+
+
+import '../global_main.css';
+import '../styles.css';
 console.log('webpack starterkit');
